7a86aa7f5e8f38a3bf30879044093e591817137c
malik <boughris@outlook.fr>
2022-12-11 19:16:13 +0000

    Removed the translation from the front end to the backend and styles

--- components/BarCodeReader.js
+++ components/BarCodeReader.js
@@ -1,56 +1,60 @@
 import { useState, useEffect } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import axios from "axios";
-import TranslateResponse from "./TranslateResponse";
 
 const BarCodeReader = (props) => {
   const [response, setResponse] = useState([]);
 
   useEffect(() => {
     const config = {
-        method: "GET",
-        url: `https://go-upc.com/api/v1/code/${props.barcode}`,
-        headers: {
-          "Content-Type": "application/json",
-          Authorization:
-            "Bearer 5872b095d1a833d0b74340b6e6354384f2883c7d3847c9bb6fa9070a6cdfbc0e",
-        },
-      };
-      axios(config)
-        .then(function (res) {
-          setResponse(res.data.product);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+      method: "GET",
+      url: `https://go-upc.com/api/v1/code/${props.barcode}`,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer 5872b095d1a833d0b74340b6e6354384f2883c7d3847c9bb6fa9070a6cdfbc0e",
+      },
+    };
+    axios(config)
+      .then(function (res) {
+        setResponse(res.data.product);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }, [props.barcode]);
 
   return (
-    <View>
-        <TranslateResponse input={response.name}/>
+    <View style={styles.container}>
+      <Text style={styles.title}>Name:</Text>
+      <Text style={styles.text}>{response.name}</Text>
+      {response.description === "No description found" ? (
+        <Text></Text>
+      ) : (
+        <>
+          <Text style={styles.title}>Description:</Text>
+          <Text style={styles.text}>{response.description}</Text>
+        </>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    display: "flex",
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+    height: 350,
+    padding: 50,
   },
-  maintext: {
-    fontSize: 16,
-    margin: 20,
+  text: {
+    fontSize: 20,
   },
-  barcodebox: {
-    alignItems: "center",
-    justifyContent: "center",
-    height: 300,
-    width: 300,
-    overflow: "hidden",
-    borderRadius: 30,
-    backgroundColor: "tomato",
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
   },
 });
 
